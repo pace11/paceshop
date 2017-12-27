6284d384d564fc3f22ae3b023c858c5178384d02
pace11 <ryanjoker87@gmail.com>
2017-12-27 18:49:33 +0700

    versi 1.0

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Kios Jersey | Pace</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+  <div class="header">
+    <p>Kios Jersey | PACE</p>
+  </div>
+  <nav>
+    <ul>
+      <li><a href="#">Beranda</a></li>
+      <li><a href="#">Belanja</a></li>
+      <li><a href="#">Tentang</a></li>
+
+      <li class="login">
+        <a href="#">Masuk</a>
+      </li>
+    </ul>
+  </nav>
+
+  <div class="box-title">
+      <p>Beranda / lel</p>
+  </div>
+  <div id="box">
+    <div class="kiri">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+    <div class="kanan">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+  </div>
+  <div class="footer">
+    <p>copyright by | pace</p>
+  </div>
+
+  </body>
+</html>
